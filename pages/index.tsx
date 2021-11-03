9f788345881fe0fda80074c5891c3c8ea819e3e1
Anatoliy Klimov <AnatoliyKlimov@Gmail.com>
2021-11-03 04:21:14 +0300

    paragraph + tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Htag } from "../components";
+import { Button, Htag, Paragraph, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -7,7 +7,12 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Текст</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='right'> Тоже Кнопка</Button>
-
+      <Paragraph size='s'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nesciunt aliquam provident tenetur itaque debitis saepe mollitia qui. Labore, illo. Earum, magni. Distinctio, nobis eius. Saepe repudiandae culpa neque quasi.</Paragraph>
+      <Paragraph size='m'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nesciunt aliquam provident tenetur itaque debitis saepe mollitia qui. Labore, illo. Earum, magni. Distinctio, nobis eius. Saepe repudiandae culpa neque quasi.</Paragraph>
+      <Paragraph size='l'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nesciunt aliquam provident tenetur itaque debitis saepe mollitia qui. Labore, illo. Earum, magni. Distinctio, nobis eius. Saepe repudiandae culpa neque quasi.</Paragraph>
+      <Tag size='m' color='ghost'>Прозрачный</Tag>
+      <Tag color='red'>Красный</Tag>
+      <Tag size='m' color='primary'>Фиолетовый</Tag>
     </>
   );
 }
