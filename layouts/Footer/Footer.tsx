5a7f29e2cd4c3887aa2ae86c04352de3d8cbc615
Anatoliy Klimov <AnatoliyKlimov@Gmail.com>
2021-11-21 23:58:11 +0300

    styles: grid/footer

--- layouts/Footer/Footer.tsx
+++ layouts/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import { FooterProps } from "./Footer.props";
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import {format} from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className,...props }: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                Next.js Portfolio {(format(new Date(), 'yyyy'))}
+            </div>
+            <a href="https://rostov.hh.ru/resume/d9a59d50ff093bfd830039ed1f6376464d7833" target='_blank'>Head Hunter</a>
+            <a href="https://github.com/AnatoliyKlimov" target='_blank'>Github</a>
+        </footer>
     );
 };
